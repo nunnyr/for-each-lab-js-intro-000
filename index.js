a0b91b0a124e0c6120fbbcf4b345e8e3136b4665
Samantha R <nunnyreyes@gmail.com>
2017-12-04 22:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function index (index, element) {
+function iterativeLog (index, element) {
   console.log(`${index}: ${element}`)
+}
+
+
+function iterate(callback) {
+  let dogs ['nico']
+  nico.forEach(callback)
+  return dogs 
+  
 }
